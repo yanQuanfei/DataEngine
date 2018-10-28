b31844ca3fa5ebd3f7d98df465068cce4c69a3ec
YanQuanfei <1042620949@qq.com>
2018-10-28 23:21:54 +0800

    添加了swagger  API 管理页面

--- DataEngine/Startup.cs
+++ DataEngine/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace DataEngine
 {
@@ -28,13 +29,19 @@ namespace DataEngine
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            #region 注入config和数据访问信息
+            #region 使用Swagger
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
 
             //services.AddOptions();
             //services.Configure<SqlContext>(Configuration.GetSection("SqlContext"));//数据库连接信息
             //services.Configure<DBSettings>(Configuration.GetSection("MongoContext"));//其他配置信息
 
-           // services.AddTransient<IRepository<LogEventData>, LogRepository>();//数据访问
+            // services.AddTransient<IRepository<LogEventData>, LogRepository>();//数据访问
             #endregion
         }
 
@@ -52,6 +59,11 @@ namespace DataEngine
 
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
